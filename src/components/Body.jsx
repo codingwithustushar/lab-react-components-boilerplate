e541af4aba08d857c2e1f75baad3d2ca1accf15e
Tushar Shekhar <tusharshekhar@Tushars-MacBook-Air.local>
2024-01-08 11:51:06 +0530

    done

---
+++ src/components/Body.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Body = (imageData) =>{
+    console.log(imageData.imageData)
+    return(
+        <div> {
+            imageData.imageData.map(image=>{
+              return(
+                <div key={image.id} className="column">
+                  <img src={image.img} alt=""></img>
+                </div>
+              )
+            })
+          }</div>
+    )
+}
+export default Body
